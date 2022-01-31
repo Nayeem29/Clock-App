f3b3e5e6ea3659cf847794391e896bfe4f83cb7a
Nayeem29 <nayeembracu29@gmail.com>
2022-02-01 01:59:34 +0600

    js-part

---
+++ main.js
@@ -0,0 +1,28 @@
+setInterval(setClock, 1000)
+
+const hour = document.querySelector('[data-hour]')
+const minute = document.querySelector('[data-minute]')
+const second = document.querySelector('[data-second]')
+
+function setClock() {
+    const currentDate = new Date()
+
+    const secondRatio = currentDate.getSeconds() / 60
+    console.log(secondRatio)
+
+    const minuteRatio = (secondRatio + currentDate.getMinutes()) / 60
+    console.log(minuteRatio)
+
+    const hourRatio = (minuteRatio + currentDate.getHours()) / 12
+    console.log(hourRatio)
+
+    setRotation(second, secondRatio)
+    setRotation(minute, minuteRatio)
+    setRotation(hour, hourRatio)
+}
+
+function setRotation(element, rotationRatio) {
+    element.style.setProperty('--rotation', rotationRatio * 360)
+}
+
+setClock()
